5c471707247a727be87b71efafbf4e95b317f890
joel <joelchiang2k@gmail.com>
2023-02-17 22:47:28 -0500

    p

--- frontend/project/App.js
+++ frontend/project/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Linkrj</Text>
+      <Text>Linkrjs</Text>
       <StatusBar style="auto" />
     </View>
   );
